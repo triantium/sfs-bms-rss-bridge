97ff802e8395524849f28a2b87aebdd3959cb9b7
Manuel Mueller <triantium@mailbox.org>
2024-10-02 10:48:57 +0200

    Update scraping target and data extraction.
    Changed the scraping URL to `https://lega.sfs-bayern.de/cgi-perl/lega-display.pl` and updated the logic to extract course details including course number, name, start, end, and availability. Introduced a new `course` struct to store and print these details.

--- main.go
+++ main.go
@@ -8,9 +8,17 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type course struct {
+	coursenumber string
+	course       string
+	start        string
+	end          string
+	free         string
+}
+
 func ExampleScrape() {
 	// Request the HTML page.
-	res, err := http.Get("http://metalsucks.net")
+	res, err := http.Get("https://lega.sfs-bayern.de/cgi-perl/lega-display.pl")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,11 +33,22 @@ func ExampleScrape() {
 		log.Fatal(err)
 	}
 
-	// Find the review items
-	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
+	// Find Courses
+	doc.Find("table.klein>tbody>tr>td[width=\"40%\"]").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the title
-		title := s.Find("a").Text()
-		fmt.Printf("Review %d: %s\n", i, title)
+		coursenumber, exists := s.Find("input[name=\"LgNr\"]").Attr("value")
+		if exists {
+			fmt.Printf("Review %d: %s\n", i, coursenumber)
+			coursename, _ := s.Find("input[name=\"Lehrgang\"]").Attr("value")
+			start, _ := s.Find("input[name=\"Beginn\"]").Attr("value")
+			end, _ := s.Find("input[name=\"Ende\"]").Attr("value")
+			free, _ := s.Find("input[name=\"Gesamt\"]").Attr("value")
+
+			c := course{coursenumber, coursename, start, end, free}
+
+			fmt.Printf("Course %d: %s\n", i, c)
+		}
+
 	})
 }
 
